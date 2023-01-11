63696460d73692b3023873f130cafe93d1eba2e3
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-01-11 10:42:13 -0600

    Lesson one notes complete

--- Notes.rb
+++ Notes.rb
@@ -164,24 +164,256 @@
 
 # -----------Symbols as hashkeys-----------
 
-american_cars =
-{
-    :chevy => 'Corvette',
-    :ford => 'Mustang',
-    :dodge => 'Challenger'
-}
+# american_cars =
+# {
+#     :chevy => 'Corvette',
+#     :ford => 'Mustang',
+#     :dodge => 'Challenger'
+# }
 
-japanese_cars =
-{
-    honda: 'Accord',
-    toyota: 'Tacoma',
-    nissan: 'Altima'
-}
+# japanese_cars =
+# {
+#     honda: 'Accord',
+#     toyota: 'Tacoma',
+#     nissan: 'Altima'
+# }
 
-puts american_cars[:ford]
-puts japanese_cars[:toyota]
+# puts american_cars[:ford]
+# puts japanese_cars[:toyota]
 
-puts american_cars.keys
-puts american_cars.values
+# puts american_cars.keys
+# puts american_cars.values
 
 
+# ------------- CONDITIONAL LOGIC --------------
+
+# ------------- Basic Conditional --------------
+
+#basic if statement
+
+# a = true
+# puts "hello" if a
+
+# puts "hello1" if true
+
+# puts "hello2" if 5 == 5
+
+# puts "hello3" if 4 != 5
+
+
+# ------------- If Else/Elseif --------------
+
+# i = 3
+
+#--- basec --- 
+# if i > 3
+#     puts"#{i} is greater then 3"
+# end
+
+# if i > 3
+#     puts"#{i} is greater then 3"
+# elsif i < 3
+#     puts"#{i} is less then 3"
+# else
+#     puts"#{i} is equal to 3"
+# end
+
+
+# ------------- Unless Statement --------------
+# opposit of if
+# a = 3
+
+# unless a == 3
+#     puts"#{a} is not equal to 3"
+# end
+
+# ------------- COMPAIRISON OPERATORS--------------
+
+# ------------- ==  -------------- not assigning value but checking for equal value
+# puts 3 == 3 #true
+# puts 3 == 5 #false
+
+
+# ------------- != -------------    checking for non equal value
+
+# puts 3 != 3 # false 
+# puts 3 != 4 # true 
+
+# ------------- > --------------
+
+# puts 3 > 3 #false
+# puts 3 > 5 #false
+# puts 5 > 3 #true
+
+# # ------------- < --------------
+
+# puts 3 < 3 #false
+# puts 3 < 5 #true
+# puts 5 < 3 #false
+
+# ------------- >= --------------
+
+# puts 3 >= 3 #true
+# puts 3 >= 5 #false
+# puts 5 >= 3 #true
+
+#  ------------- <= --------------
+
+# puts 3 <= 3 #true
+# puts 3 <= 5 #true
+# puts 5 <= 3 #false
+
+# ------------- <=> --------------
+
+# 1 <=> 2  # -1
+# 2 <=> 2  # 0
+# 3 <=> 2  # 1
+
+# -1 if the first value is less than the second.
+# 0 if the first value is equal to the second.
+# 1 if the first value is greater than the second.
+
+
+# ------------- LOGICAL OPERATORS --------------
+# a = 6
+# ------------- && --------------
+
+# puts "#{a} is between 4 and 10" if a > 4 && a < 10
+
+# if a > 4 && a < 10
+#     puts "#{a} is between 4 and 10" 
+# end
+ 
+# ------------- || --------------
+
+# puts "#{a} is between 4 and 10" if a > 4 || a < 10
+
+# if a > 4 || a < 10
+#     puts "#{a} is either greater then  4 OR less then 10" 
+# end
+
+# ------------- Ternary Operators --------------
+
+# can_swim = false
+
+# response = can_swim ? "You wont drown" : "You Might Drown"
+
+# puts response
+
+# #response is false so prints "you might drown"
+
+# condition ? if_true_expression : if_false_expression
+
+# x = 5
+# y = 10
+
+# # Using ternary operator
+# result = x > y ? "x is greater than y" : "x is less than or equal to y"
+
+
+# ------------- Case Statement --------------
+# grade = 'A'
+
+# case grade
+# when "A"
+#     puts "Good Job!"
+# when "B"
+#     puts "Good job, try harder next time!"
+# when "C"
+#     puts "You Passed but study more!"
+# when "D"
+#     puts "Almost Passed. Study more"
+# else "F"
+#     puts "You failed, please study"
+# end
+
+#                                         ------------- END CONDITIONAL LOGIC --------------
+
+#                                         ------------- LOOPS --------------
+
+# ------------- Generic Loops -------------- 
+# i = 0
+
+# loop do
+#     puts "i is #{i}"
+#     i += 1              # same as i = i + 1
+#     break if i == 10
+# end
+
+# Infinite Loop
+# loop do
+#     puts "i is #{i}"
+# end
+
+# ------------- While Loops -------------- 
+# less code 
+# i = 0
+
+# while i < 10 do
+#     puts "i is #{i}"
+#     i += 1  
+# end
+
+# ------------- Ultil Loops -------------- 
+# The loop will continue executing the code inside as long as the condition is false.
+# Once the condition becomes true, the loop will exit and the program will continue 
+# executing the code that follows the loop.
+
+# i = 0
+
+# until i == 10
+#     puts "i is #{i}"
+#     i += 1  
+# end
+
+# ------------- Ranges --------------   
+# Ranges are represented using the .. (inclusive) or ... (exclusive) operator.
+# An inclusive range is defined using the .. operator and includes the 
+# start and end values of the range. For example, 1..10 defines a range
+#  of numbers from 1 to 10, including both 1 and 10.
+
+# An exclusive range is defined using the ... operator and includes all
+#  the values up to, but not including, the end value. For example, 
+#  1...10 defines a range of numbers from 1 to 9, not including 10.
+
+# i = 0
+
+# (1..10).each do |i|
+#     puts i
+# end
+# # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# (1...10).each do |i|
+#     puts i
+# end
+# # [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# ------------- For Loops --------------   
+
+# for i in 0..10
+#     puts "#{i} zombies incoming!"
+# end
+
+
+# ------------- Times Loops --------------  
+
+# 5.times do 
+#     puts "hello world"
+# end
+
+5.downto(1) do |i|
+    puts i
+  end
+
+# In summary, the |i| notation is used to define variables to 
+# be used as block parameters. It makes your code more readable
+# and easy to understand. It is passed to the block and can be 
+# used inside the block as needed, especially in cases like 
+# counting iterations as in the case of times loop.
+
+
+
+
+
+#                                         ------------- END LOOPS --------------
